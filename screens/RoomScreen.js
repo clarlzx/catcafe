0f696b71bf7e68ee1694452d1427728e7f876621
clarlzx <clarlzx@gmail.com>
2021-07-24 04:12:22 +0800

    Prevent users from exiting with back button

--- screens/RoomScreen.js
+++ screens/RoomScreen.js
@@ -1,31 +1,69 @@
-import React, {useState} from "react";
-import { View, Text, StyleSheet, TouchableOpacity, FlatList } from "react-native";
-import firebase from '../database/firebaseDB';
-import Cat from '../components/Cat';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Alert,
+} from "react-native";
+import firebase from "../database/firebaseDB";
+import Cat from "../components/Cat";
+import { useBackHandler } from "@react-native-community/hooks";
 
 export default function RoomScreen({ navigation, route }) {
   const { id } = route.params;
 
-  const [userData,setUserData] = useState([]);
+  const [userData, setUserData] = useState([]);
 
-  firebase.firestore().collection("rooms").doc("iu0TUc").get().then((doc) => {
-    setUserData(doc.data().userData);
-  })
+  useBackHandler(() => {
+    if (navigation.isFocused()) {
+      Alert.alert("Invalid action", "Please exit using the exit button", [
+        {
+          text: "OK",
+        },
+      ]);
+      return true;
+    } else {
+      return false;
+    }
+  });
 
-//   const userData = [{catName: "Tim", catType: "calico", userName: "Anon"},
-// {catName: "MM", catType: "royal", userName: "Hey"},
-// {catName: "meimei", catType: "black", userName: "Hello"}];
+  firebase
+    .firestore()
+    .collection("rooms")
+    .doc("iu0TUc")
+    .get()
+    .then((doc) => {
+      setUserData(doc.data().userData);
+    });
+
+  //   const userData = [{catName: "Tim", catType: "calico", userName: "Anon"},
+  // {catName: "MM", catType: "royal", userName: "Hey"},
+  // {catName: "meimei", catType: "black", userName: "Hello"}];
 
   return (
     <View style={styles.container}>
-      <Text style={{padding: 40}}>Hi I am a room where cats roam</Text>
-      
+      <Text style={{ padding: 40 }}>Hi I am a room where cats roam</Text>
+
       <FlatList
         data={userData}
-        renderItem={({item}) => <View>
-          <Text style={{textAlign: "center"}}>{item.username}'s cat {item.catName}</Text>
-          <Cat catName={item.catType} style={{padding: 5, height: 250, width: 150, transform:[{scale: 0.7}]}}/>
-          </View>}
+        renderItem={({ item }) => (
+          <View>
+            <Text style={{ textAlign: "center" }}>
+              {item.username}'s cat {item.catName}
+            </Text>
+            <Cat
+              catName={item.catType}
+              style={{
+                padding: 5,
+                height: 250,
+                width: 150,
+                transform: [{ scale: 0.7 }],
+              }}
+            />
+          </View>
+        )}
         numColumns={2}
         styles={styles.list}
       />
@@ -70,5 +108,5 @@ const styles = StyleSheet.create({
   },
   list: {
     paddingTop: 100,
-  }
+  },
 });
